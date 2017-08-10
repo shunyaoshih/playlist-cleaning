8b93f6a1c7a83875410baa4fba9bea0f1eb7400c
Gary Shih <garyshih@KKBOXde-MacBook-Pro.local>
2017-08-10 12:23:39 +0800

    precision and recall@n

--- evaluate.py
+++ evaluate.py
@@ -14,6 +14,26 @@ def cal_precision(true_positives, false_positives):
 def cal_recall(true_positives, false_negatives):
     return (true_positives / (true_positives + false_negatives))
 
+def general_metrics(ans_txt, nn_txt, n):
+    true_positives = 0
+    false_positives = 0
+    false_negatives = 0
+
+    for i in range(len(nn_txt)):
+        limit = min([n, len(nn_txt[i])])
+        for j in range(limit):
+            if nn_txt[i][j] in ans_txt[i]:
+                true_positives += 1
+            else:
+                false_positives += 1
+    for i in range(len(ans_txt)):
+        limit = min([n, len(ans_txt[i])])
+        for j in range(limit):
+            if ans_txt[i][j] not in nn_txt[i]:
+                false_negatives += 1
+    print('precision@{}: {}'.format(n, cal_precision(true_positives, false_positives)))
+    print('recall@{}: {}'.format(n, cal_recall(true_positives, false_negatives)))
+
 if __name__ == "__main__":
     para = params_setup()
 
@@ -23,21 +43,9 @@ if __name__ == "__main__":
     nn_path = './results/' + para.nn + '_out.txt'
     nn_txt = read_file(nn_path)
 
-    true_positives = 0
-    false_positives = 0
-    false_negatives = 0
-
-    for i in range(len(nn_txt)):
-        for j in range(len(nn_txt[i])):
-            if nn_txt[i][j] in ans_txt[i]:
-                true_positives += 1
-            else:
-                false_positives += 1
-    for i in range(len(ans_txt)):
-        for j in range(len(ans_txt[i])):
-            if ans_txt[i][j] not in nn_txt[i]:
-                false_negatives += 1
 
     print('{}\'s result:'.format(para.nn))
-    print('precision: {}'.format(cal_precision(true_positives, false_positives)))
-    print('recall: {}'.format(cal_recall(true_positives, false_negatives)))
+    for i in range(10, 60, 10):
+        general_metrics(ans_txt, nn_txt, i)
+        print()
+    general_metrics(ans_txt, nn_txt, 300)
